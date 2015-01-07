cd2d03eb70137738391569b1b45595203e049fe7
Near <nearsy.h@gmail.com>
2015-01-06 20:02:15 -0500

    clean forwarderpools, it is not a 'pool'; remove old pools implementations

--- src/com/y59song/Forwader/ForwarderPools.java
+++ src/com/y59song/Forwader/ForwarderPools.java
@@ -21,7 +21,6 @@ package com.y59song.Forwader;
 
 import com.y59song.LocationGuard.MyVpnService;
 import com.y59song.Network.IP.IPDatagram;
-import com.y59song.Utilities.RealPool.BoundedBlockingPool;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -30,32 +29,15 @@ import java.util.HashMap;
  * Created by frank on 2014-04-01.
  */
 public class ForwarderPools {
-  private final String TAG = ForwarderPools.class.getSimpleName();
-  private BoundedBlockingPool<UDPForwarder> udpForwarderPool;
-  private BoundedBlockingPool<TCPForwarder> tcpForwarderPool;
-  private static final int udpPoolSize = 50;
-  private static final int tcpPoolSize = 100;
   private HashMap<Integer, AbsForwarder> portToForwarder;
   private MyVpnService vpnService;
 
   public ForwarderPools(MyVpnService vpnService) {
     this.vpnService = vpnService;
-
-    /*
-    udpForwarderPool = new BoundedBlockingPool<UDPForwarder>(
-      udpPoolSize,
-      new ForwarderValidator(),
-      new UDPForwarderFactory(vpnService));
-    tcpForwarderPool = new BoundedBlockingPool<TCPForwarder>(
-      tcpPoolSize,
-      new ForwarderValidator(),
-      new TCPForwarderFactory(vpnService));
-      */
     portToForwarder = new HashMap<Integer, AbsForwarder>();
   }
 
   public AbsForwarder get(int port, byte protocol) {
-    //Log.d(TAG, "GET : " + udpForwarderPool.getSize() + ", " + tcpForwarderPool.getSize());
     if(portToForwarder.containsKey(port) && !portToForwarder.get(port).isClosed())
       return portToForwarder.get(port);
     else {
@@ -67,16 +49,7 @@ public class ForwarderPools {
   }
 
   private AbsForwarder getByProtocol(byte protocol) {
-    /*
     switch(protocol) {
-      case IPDatagram.TCP : TCPForwarder temp = tcpForwarderPool.get(); new Thread(temp).start(); return temp;
-      case IPDatagram.UDP : return udpForwarderPool.get();
-      default:
-        return null;
-    }
-    */
-    switch(protocol) {
-      //case IPDatagram.TCP : TCPForwarder temp = new TCPForwarder(vpnService); new Thread(temp).start(); return temp;
       case IPDatagram.TCP : return new TCPForwarder(vpnService);
       case IPDatagram.UDP : return new UDPForwarder(vpnService);
       default: return null;
@@ -84,14 +57,10 @@ public class ForwarderPools {
   }
 
   public void release(UDPForwarder udpForwarder) {
-    //Log.d(TAG, "Release : " + udpForwarderPool.getSize() + ", " + tcpForwarderPool.getSize());
-    //udpForwarderPool.release(udpForwarder);
     portToForwarder.values().removeAll(Collections.singleton(udpForwarder));
   }
 
   public void release(TCPForwarder tcpForwarder) {
-    //Log.d(TAG, "Release : " + udpForwarderPool.getSize() + ", " + tcpForwarderPool.getSize());
-    //tcpForwarderPool.release(tcpForwarder);
     portToForwarder.values().removeAll(Collections.singleton(tcpForwarder));
   }
 }
--- src/com/y59song/Utilities/Pool/ObjectFactory.java
+++
@@ -1,8 +0,0 @@
-package com.y59song.Utilities.Pool;
-
-/**
- * Created by y59song on 28/05/14.
- */
-public interface ObjectFactory<T> {
-  public abstract T createNew();
-}
